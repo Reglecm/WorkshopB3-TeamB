956b912f141fccc21e8cc3c8b819bf2200671a61
Guillaume <garciaguillaume18@gmail.com>
2018-10-01 15:59:10 +0200

    front page

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 console.log("main.js is ready ");
 
+
+$(document).on("scroll", function(){
+    if
+  ($(document).scrollTop() > 86){
+      $("#banner").addClass("shrink");
+    }
+    else
+    {
+        $("#banner").removeClass("shrink");
+    }
+});
