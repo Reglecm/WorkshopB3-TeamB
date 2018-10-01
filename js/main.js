f439d004f47ae489b63da06a4cca9da4677dcb11
Guillaume <garciaguillaume18@gmail.com>
2018-10-01 20:08:25 +0200

    VR STV

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-console.log("main.js is ready ");
-
-$(document).ready(function(){
-
+$(document).ready(function () 
+{
     $("#banner").hide();
+    console.log("main.js is ready ");
     
 })
-	$(document).on("scroll", function(){
-		if
-      ($(document).scrollTop() > 1){
-          $("#banner").addClass("shrink");
-          $("#banner").show();
-		}
-		else
-		{
-            $("#banner").hide();
-			$("#banner").removeClass("shrink");
-		}
-	});
+
+$(document).on("scroll", function () {
+   
+    if ($(document).scrollTop() > 100)
+     {
+        $("#banner").addClass("shrink");
+        $("#banner").slideDown(400);
+    } else 
+    {
+        $("#banner").slideUp(400)
+    }
+
+});
