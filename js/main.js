ea3c4163623afc07c1e802be89cff5392f07c800
Guillaume <garciaguillaume18@gmail.com>
2018-10-03 12:02:24 +0200

    logo epsi

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
-$(document).ready(function () 
-{
+$(document).ready(function () {
     $("#banner").hide();
     console.log("main.js is ready ");
-    
+
+    $('.TRightGreen').hover(function () {
+        $(this).css('transform', 'translateX(50px)')
+    })
+
+    $('.TRightGreen').mouseleave(function () {
+        $(this).css('transform', 'unset');
+    })
+
 })
 
 $(document).on("scroll", function () {
-   
-    if ($(document).scrollTop() > 100)
-     {
+
+    if ($(document).scrollTop() > 100) {
         $("#banner").addClass("shrink");
         $("#banner").slideDown(400);
-    } else 
-    {
+    } else {
         $("#banner").slideUp(400)
     }
 
